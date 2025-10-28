2f31e4e5a57648018798bb4e73d977af64cdc2a2
vikrantPandeyGH <vikrantpandey267@gmail.com>
2025-10-29 00:00:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,14 @@ app.use(async (req, res, next) => {
   }
 });
 
-app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // ya specific frontend URL
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
